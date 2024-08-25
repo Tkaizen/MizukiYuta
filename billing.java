4fcafaf2ca25b0b8ced10a1864be063d2244b40f
Mizuki <163004613+MizukiYuta@users.noreply.github.com>
2024-08-26 01:24:17 +0800

    Create billing.java

---
+++ billing.java
@@ -0,0 +1,12 @@
+price = float(input("enter price"))
+quantity = int(input("enter quantity")) 
+
+total = price + quantity
+
+print("the total amount is"+str(total))
+
+cash = int(input("enter cash"))
+
+change = cash = total
+
+print("your change is"+str(change))
